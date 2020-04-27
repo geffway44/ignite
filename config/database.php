7f6d71bf3ad734ca42523e8485742ff1afde3458
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-04-27 08:08:17 +0530

    WIP

--- config/database.php
+++ config/database.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Support\Str;
 
-$redisUrl = parse_url(env('REDIS_URL'));
-
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -36,7 +33,6 @@ return [
     */
 
     'connections' => [
-
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
@@ -91,7 +87,6 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
-
     ],
 
     /*
@@ -119,21 +114,17 @@ return [
     */
 
     'redis' => [
-
         'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
-            'prefix' => Str::slug(env('APP_NAME', 'laravel'), '_').'_database_',
+            'prefix' => Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_',
         ],
 
         'default' => [
-            'host' => $redisUrl['host'],
-            'password' => $redisUrl['pass'],
-            'port' => $redisUrl['port'],
-            // 'host' => env('REDIS_HOST', '127.0.0.1'),
-            // 'password' => env('REDIS_PASSWORD', null),
-            // 'port' => env('REDIS_PORT', 6379),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
         ],
 
@@ -143,7 +134,5 @@ return [
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_CACHE_DB', 1),
         ],
-
     ],
-
 ];
