4b3ccfb94298257a553b5850a2bf0dbe52066a08
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-04 10:26:13 +0530

    Add update reply tests

--- tests/Feature/Replies/UpdateReplyTest.php
+++ tests/Feature/Replies/UpdateReplyTest.php
@@ -2,21 +2,54 @@
 
 namespace Tests\Feature\Replies;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Reply;
+use App\Models\Thread;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UpdateReplyTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    public function testAReplyCanBeUpdated()
     {
-        $response = $this->get('/');
+        $thread = create(Thread::class);
+        $reply = create(Reply::class, ['thread_id' => $thread->id]);
+
+        $response = $this->signIn($reply->user)->put(route('replies.update', [
+            'thread' => $thread,
+            'reply' => $reply,
+        ]), ['body' => 'updated reply body.']);
+
+        $response->assertStatus(303);
+        $this->assertEquals($reply->refresh()->body, 'updated reply body.');
+    }
+
+    public function testAReplyCanBeUpdatedThroughJsonRequest()
+    {
+        $thread = create(Thread::class);
+        $reply = create(Reply::class, ['thread_id' => $thread->id]);
+
+        $response = $this->signIn($reply->user)->putJson(route('replies.update', [
+            'thread' => $thread,
+            'reply' => $reply,
+        ]), ['body' => 'updated reply body.']);
 
         $response->assertStatus(200);
+        $this->assertEquals($reply->refresh()->body, 'updated reply body.');
+    }
+
+    public function testAValidBodyIsRequired()
+    {
+        $thread = create(Thread::class);
+        $reply = create(Reply::class, ['thread_id' => $thread->id]);
+
+        $response = $this->signIn($reply->user)->put(route('replies.update', [
+            'thread' => $thread,
+            'reply' => $reply,
+        ]), ['body' => '']);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['body']);
     }
 }
