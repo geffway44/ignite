09bdee1dcbee5e97a7158bc3ff29d40ae665d6be
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-08-03 00:22:28 +0530

    Fix analysis issues

--- app/Actions/Auth/Traits/PasswordUpdater.php
+++ app/Actions/Auth/Traits/PasswordUpdater.php
@@ -2,30 +2,33 @@
 
 namespace App\Actions\Auth\Traits;
 
-use Illuminate\Support\Str;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Str;
 
 trait PasswordUpdater
 {
     /**
      * Update given user password field.
      *
-     * @param \Illuminate\Contracts\Auth\Authenticatable $user
-     * @param string                                     $password
-     * @param bool                                       $withoutRemember
+     * @param \App\Models\User $user
+     * @param string           $password
+     * @param bool             $withoutRemember
      *
      * @return void
      */
-    protected function updatePassword(Authenticatable $user, string $password, bool $withoutRemember = false): void
-    {
+    protected function updatePassword(
+        User $user,
+        string $password,
+        bool $withoutRemember = false
+    ): void {
         DB::transaction(function () use ($user, $password, $withoutRemember) {
-            $user->forceFill(
-                array_merge([
-                    'password' => Hash::make($password),
-                ], $withoutRemember ? [] : ['remember_token' => Str::random(60)])
-            )->save();
+            $user->forceFill(array_merge([
+                'password' => Hash::make($password),
+            ], $withoutRemember ? [] : [
+                'remember_token' => Str::random(60),
+            ]))->save();
         });
     }
 }
