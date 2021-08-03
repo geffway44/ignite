cc486d17840693a45ad6876358ad0e4f33c58347
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-08-03 14:45:11 +0530

    Add thorough test for browsing threads

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -14,7 +14,7 @@ class ThreadController extends Controller
      */
     public function index()
     {
-        return response('Hello World', 200);
+        return Thread::all();
     }
 
     /**
@@ -24,62 +24,61 @@ class ThreadController extends Controller
      */
     public function create()
     {
-        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Thread  $thread
+     * @param \App\Models\Thread $thread
+     *
      * @return \Illuminate\Http\Response
      */
     public function show(Thread $thread)
     {
-        //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Thread  $thread
+     * @param \App\Models\Thread $thread
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit(Thread $thread)
     {
-        //
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Thread  $thread
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Thread       $thread
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Thread $thread)
     {
-        //
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Thread  $thread
+     * @param \App\Models\Thread $thread
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy(Thread $thread)
     {
-        //
     }
 }
--- tests/Feature/ThreadTest.php
+++ tests/Feature/ThreadTest.php
@@ -3,9 +3,8 @@
 namespace Tests\Feature;
 
 use App\Models\Thread;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class ThreadTest extends TestCase
@@ -13,12 +12,18 @@ class ThreadTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
-    public function browse_all_threads()
+    public function browseAllThreads()
     {
-//        $thread = Thread::factory()->make();
+        $threads = Thread::factory()->count(10)->create();
 
         $response = $this->get('/threads');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200); // assert response is successful
+        $response->assertJson(fn (AssertableJson $json) => $json->has(10)); // assert response content has 10 items in it.
+        $content = $response->getContent(); // get the response content
+        $threads->each(function ($thread) use ($content) {
+            // assert each thread is in the response content
+            $this->assertStringContainsString($thread->title, $content);
+        });
     }
 }
